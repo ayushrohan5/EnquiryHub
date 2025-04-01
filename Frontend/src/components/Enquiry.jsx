771f1b0c5435900a12170ff4d43e79b249bc94fe
Ayush Rohan <ayushrohan5@gmail.com>
2025-04-01 15:32:33 +0530

    2nd push

---
+++ Frontend/src/components/Enquiry.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Enquiry = () => {
+  return (
+    <>
+    <h1 className='text-[40px] text-center py-6 font-bold'>User Enquiry</h1>
+
+    <div className = 'grid grid-cols-[30%_auto]'>
+
+        <div className='bg-gray-200 p-4'>
+            <h2 className = 'text-[20px] font-bold'>User Enquiry</h2>
+
+
+        </div>
+
+
+
+
+
+
+        </div>
+         
+    </>
+  )
+}
+
+export default Enquiry
